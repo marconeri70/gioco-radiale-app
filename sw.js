a2b07f42cfbe21cf1c011e885bb162558ddd0d79
marconeri70 <marconeri70@gmail.com>
2025-09-19 18:12:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
-const CACHE='skf-gr-v3.7';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./script.js','./manifest.json','./skf-192.png','./favicon.ico'])))}) 
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE='skf-gr-v3.7.1';
+const ASSETS=['./','./index.html','./manifest.json','./skf-192.png','./skf-512.png','./favicon.ico'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
